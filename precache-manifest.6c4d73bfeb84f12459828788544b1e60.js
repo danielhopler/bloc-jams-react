ae3df18ae42bf5cb12b099bcdd1c5525653e9970
Daniel Hopler <hopler.daniel@gmail.com>
2019-05-14 19:10:50 -0400

    Updates

---
+++ precache-manifest.6c4d73bfeb84f12459828788544b1e60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2643e44d3e9dc93a5dd98409877c5f66",
+    "url": "/react-bloc-jams/index.html"
+  },
+  {
+    "revision": "741545bff8a416d14030",
+    "url": "/react-bloc-jams/static/css/main.aabc0d29.chunk.css"
+  },
+  {
+    "revision": "70f5f2473f306996c842",
+    "url": "/react-bloc-jams/static/js/2.a3864955.chunk.js"
+  },
+  {
+    "revision": "741545bff8a416d14030",
+    "url": "/react-bloc-jams/static/js/main.00c81b71.chunk.js"
+  },
+  {
+    "revision": "85324ec41d0294b0d47b",
+    "url": "/react-bloc-jams/static/js/runtime~main.f6df10e1.js"
+  }
+]);
